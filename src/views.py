3c100a523f730a921aec8ef2a88c9443c1efb339
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2024-02-22 21:56:01 +0100

    elegancko wykonane endpointy

--- src/views.py
+++ src/views.py
@@ -1,35 +1,48 @@
 from flask import Flask
+from flask import request
 from flask import jsonify
+from logic import get_all_users
+from logic import get_specific_user
+from logic import create_user
+from logic import change_user_data
+from logic import remove_user
 
 
 app = Flask(__name__)
 
-users = []
-
 
 @app.get('/users')
 def get_users():
-    return jsonify(users)
+    return jsonify(get_all_users()), 200
 
 
-@app.get('/users/<int:id>')
-def get_user(id):
-    pass
+@app.get('/users/<int:user_id>')
+def get_user(user_id):
+    user = get_specific_user(user_id)
+    if user:
+        return jsonify(user), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 
 @app.post('/users')
 def create_user():
-    pass
+    user_data = request.get_json()
+    create_user(user_data)
+    return jsonify(user_data), 201
 
 
-@app.patch('/users/<int:id>')
-def change_user_data(id):
-    user = requests.json
-    users[id] = user
-    return jsonify(user)
+@app.patch('/users/<int:user_id>')
+def change_user_data(user_id):
+    user_data = request.get_json()
+    user = change_user_data(user_id, user_data)
+    if user:
+        return jsonify(user), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 
-@app.delete('/users/<int:id>')
-def delete_user(id):
-    user = users.pop(id)
-    return user
+@app.delete('/users/<int:user_id>')
+def delete_user(user_id):
+    remove_user(user_id)
+    return '', 204
