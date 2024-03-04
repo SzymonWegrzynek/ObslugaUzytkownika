b8eb0585c3bdd5c86cbd06b7d5525e0d5a5b1bdb
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2024-03-04 12:56:42 +0100

    zmiana na klasy - bardziej obiektowo

--- src/logic.py
+++ src/logic.py
@@ -1,25 +1,21 @@
-from persistence import get_users
-from persistence import get_user_by_id
-from persistence import add_user
-from persistence import update_user
-from persistence import delete_user
+from persistence import UserRepository
 
 
-def get_all_users():
-    return get_users()
+class UserLogic:
+    def __init__(self):
+        self.user_repository = UserRepository()
 
+    def get_all_users(self):
+        return self.user_repository.get_users()
 
-def get_specific_user(user_id):
-    return get_user_by_id(user_id)
+    def get_specific_user(self, user_id):
+        return self.user_repository.get_user_by_id(user_id)
 
+    def create_user(self, user_data):
+        self.user_repository.add_user(user_data)
 
-def create_user(user_data):
-    add_user(user_data)
+    def change_user_data(self, user_id, user_data):
+        return self.user_repository.update_user(user_id, user_data)
 
-
-def change_user_data(user_id, user_data):
-    return update_user(user_id, user_data)
-
-
-def remove_user(user_id):
-    delete_user(user_id)
+    def remove_user(self, user_id):
+        self.user_repository.delete_user(user_id)
--- src/persistence.py
+++ src/persistence.py
@@ -1,28 +1,25 @@
-users = []
+class UserRepository:
+    def __init__(self):
+        self.users = []
 
+    def get_users(self):
+        return self.users
 
-def get_users():
-    return users
+    def get_user_by_id(self, user_id):
+        for user in self.users:
+            if user['id'] == user_id:
+                return user
+        return None
 
+    def add_user(self, user):
+        self.users.append(user)
 
-def get_user_by_id(user_id):
-    for user in users:
-        if user['id'] == user_id:
-            return user
-    return None
+    def update_user(self, user_id, user_data):
+        for user in self.users:
+            if user['id'] == user_id:
+                user.update(user_data)
+                return user
+        return None
 
-
-def add_user(user):
-    users.append(user)
-
-
-def update_user(user_id, user_data):
-    for user in users:
-        if user['id'] == user_id:
-            user.update(user_data)
-            return user
-    return None
-
-
-def delete_user(user_id):
-    users[:] = [user for user in users if user['id'] != user_id]
+    def delete_user(self, user_id):
+        self.users[:] = [user for user in self.users if user['id'] != user_id]
